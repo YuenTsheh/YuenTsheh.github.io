ecfcf39e39b5fa75693a74761388c32ddc660f83
Darian <1934849492@qq.com>
2019-07-14 13:07:20 +0800

    ...

--- js/jquery.nav.js
+++ js/jquery.nav.js
@@ -221,4 +221,17 @@
 		});
 	};
 
+	$('#log-wechat-darian').on('mousemove', function(ev) {
+	var left = ev.clientX + 10
+	var top = ev.clientY + 10
+	$('#log-wechat-darian-img').css({
+	    top: top + 'px',
+	    left: left + 'px',
+	    display: 'block'
+	})
+	})
+	$('#log-wechat-darian').on('mouseout', function() {
+	    $('#log-wechat-darian-img').css('display', 'none')
+	})
+
 })( jQuery, window , document );
